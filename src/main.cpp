a415457ac7634064e3f6947c8823fd6c9374b926
m1cha1s <msulej04@gmail.com>
2022-10-04 18:29:16 +0200

    Port fully moved

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,41 @@
 #include <stdio.h>
+#include <miniSystem.hpp>
+#include <console.hpp>
+#include <stream.hpp>
+#include <string>
+
+static void fault_handler(void *, const char *msg)
+{
+    fprintf(stderr, "FATAL: %s\n", (msg ? msg : "no message."));
+    fflush(stderr);
+    exit(1);
+}
+
+class ConsoleIf : public StreamIf<std::string>
+{
+public:
+    bool readable = false;
+    bool writable = true;
+
+    ConsoleIf() {}
+    ~ConsoleIf() {}
+
+    void write(const std::string &src)
+    {
+        printf("%s\n", src.c_str());
+    }
+
+    void read(std::string &dst)
+    {
+    }
+};
 
 int main()
 {
-    printf("Hello world!\n");
+
+    consoleStream.init(new ConsoleIf());
+
+    MiniSystem ms(fault_handler);
+
+    printf("Result: %d.\n", ms.eval("console.log('Hello world!')"));
 }
