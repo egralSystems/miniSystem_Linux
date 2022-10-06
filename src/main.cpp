745c76b545d0ad1f55c6ea1446af19b6ed1eb84f
m1cha1s <msulej04@gmail.com>
2022-10-06 18:01:16 +0200

    Implemented FS

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 
 #include <miniSystem.hpp>
 #include <stream.hpp>
+#include <fs.hpp>
 
 class ConsoleIf : public StreamIf<std::string>
 {
@@ -23,9 +24,63 @@ public:
     }
 };
 
+class FSIf : public FileIf
+{
+private:
+    FILE *file;
+
+public:
+    FSIf(){};
+    ~FSIf(){};
+
+    void open(std::string path, std::string accessType)
+    {
+        file = fopen(path.c_str(), accessType.c_str());
+    }
+
+    void close()
+    {
+        if (file)
+            fclose(file);
+    }
+
+    std::string read(int charCount)
+    {
+        std::string buf(charCount, '\0');
+
+        fread(&buf[0], sizeof(char), charCount, file);
+
+        return buf;
+    }
+
+    void write(std::string toWrite)
+    {
+        fwrite(toWrite.c_str(), toWrite.size(), 0, file);
+    }
+
+    int size()
+    {
+        int size = 0;
+
+        if (file)
+        {
+            fseek(file, 0L, SEEK_END);
+            size = ftell(file);
+            fseek(file, 0L, SEEK_SET);
+        }
+
+        return size;
+    }
+
+    void seek(int pos)
+    {
+        fseek(file, pos, SEEK_SET);
+    }
+};
+
 int main()
 {
-    MiniSystem ms(new ConsoleIf);
+    MiniSystem<ConsoleIf, FSIf> ms = MiniSystem<ConsoleIf, FSIf>();
 
     printf("Result: %d.\n", ms.eval("System.print(\"Hello world!\")"));
 }
